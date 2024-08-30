468feeb3c2cfdd4e53f1be69f02d6c4b710277e1
Park_SinYeong <60610390+zachpaul7@users.noreply.github.com>
2024-08-30 17:46:21 +0900

    Update GameMamager.cs

--- #02 Singleton/GameMamager.cs
+++ #02 Singleton/GameMamager.cs
@@ -1,12 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+    
+// Singleton<T>로 상속받아서 사용
 public class GameMamager : Singleton<GameMamager>
 {
-    
-    // Singleton<T>로 상속받아서 사용
-
     private void Awake()
     {
         if(Instance != null && Instance != this)
